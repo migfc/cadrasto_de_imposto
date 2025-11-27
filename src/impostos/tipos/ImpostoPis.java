59096e7904149873d7f8b26f281d4e61be0c72ad
Miguel de Freitas Comibra <miguel.freitas.coimbra18@gmail.com>
2025-11-27 15:27:42 -0300

    implementando a interfase Imposto na clase ImpostoPis

--- src/impostos/tipos/ImpostoPis.java
+++ src/impostos/tipos/ImpostoPis.java
@@ -1,5 +1,19 @@
 package impostos.tipos;
 
-public class ImpostoPis {
+import impostos.interfase.Imposto;
+
+public class ImpostoPis implements Imposto {
+
+    @Override
+    public double calcular() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'calcular'");
+    }
+
+    @Override
+    public String getDescricao() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getDescricao'");
+    }
 
 }
