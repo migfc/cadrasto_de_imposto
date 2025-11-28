7cd2d053253d7007b4e91229b323c439f19a1382
Miguel de Freitas Comibra <miguel.freitas.coimbra18@gmail.com>
2025-11-28 19:49:34 -0300

    Commit 2: Adicionar menu principal e loop do programa

--- src/App.java
+++ src/App.java
@@ -7,5 +7,22 @@ public class App {
         System.out.print("Digite o nome da empresa: ");
         String nomeEmpresa = scanner.nextLine();
         var pagamentos = new Pagamentos(nomeEmpresa);
+        boolean condition = true;
+        do {
+            System.out.println("Menu de Impostos");
+            System.out.println("1. Adicionar Imposto IPI");
+            System.out.println("2. adicionar Imposto PIS");
+            System.out.println("3. listar Impostos");
+            System.out.println("0. sair");
+            System.out.print("Escolha uma opcao: ");
+            int opcao = scanner.nextInt();
+            switch (opcao) {
+                case 0:
+                    condition = false;
+                    break;
+            }
+        } while (condition);
+        System.out.println("Encerando a aplicação");
+        scanner.close();
     }
 }
