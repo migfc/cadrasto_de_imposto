34a55b2a9fe390f8e67aaa242cf04fef7333a8f3
Miguel de Freitas Comibra <miguel.freitas.coimbra18@gmail.com>
2025-11-28 18:15:01 -0300

    adicionando a leitura do nome da empresa na classe App

--- src/App.java
+++ src/App.java
@@ -1,7 +1,11 @@
+import pagamentos.Pagamentos;
 
 public class App {
     public static void main(String[] args) {
         var scanner = new java.util.Scanner(System.in);
+        System.out.print("Digite o nome da empresa: ");
+        String nomeEmpresa = scanner.nextLine();
+        Pagamentos pagamentos = new Pagamentos(nomeEmpresa);
         scanner.close();
     }
 }
