d191b06c6c7e0fe84a77865343c1c8638db04807
Miguel de Freitas Comibra <miguel.freitas.coimbra18@gmail.com>
2025-11-28 18:08:04 -0300

    adicionando o metodo adicionarImposto na clase Pagamentos

--- src/pagamentos/Pagamentos.java
+++ src/pagamentos/Pagamentos.java
@@ -1,6 +1,7 @@
 package pagamentos;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import impostos.interfase.Imposto;
 
@@ -11,4 +12,9 @@ public class Pagamentos {
     public Pagamentos(String nomeEmpresa) {
         this.nomeEmpresa = nomeEmpresa;
     }
+
+    public void adicionarImposto(Imposto imposto) {
+        impostos.add(imposto);
+    }
+
 }
