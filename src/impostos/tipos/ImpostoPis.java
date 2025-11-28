81db303fa50568d5b7f65548bd44de846523d8b1
Miguel de Freitas Comibra <miguel.freitas.coimbra18@gmail.com>
2025-11-28 16:23:32 -0300

    adicionando o contrutor

--- src/impostos/tipos/ImpostoPis.java
+++ src/impostos/tipos/ImpostoPis.java
@@ -6,7 +6,12 @@ public class ImpostoPis implements Imposto {
     private String descricao;
     private double debito;
     private double credito;
-    
+
+    public ImpostoPis(String descricao, double debito, double credito) {
+        this.descricao = descricao;
+        this.debito = debito;
+        this.credito = credito;
+    }
     @Override
     public double calcular() {
         // TODO Auto-generated method stub
