75385ea37a70a0f8359a74a1206a8f34c60a16c9
Miguel de Freitas Comibra <miguel.freitas.coimbra18@gmail.com>
2025-11-28 17:57:18 -0300

    adicionando o construtor da clase ImpostoIpi

--- src/impostos/tipos/ImpostoIpi.java
+++ src/impostos/tipos/ImpostoIpi.java
@@ -9,6 +9,14 @@ public class ImpostoIpi implements Imposto {
     private double seguro;
     private double outrasDespesas;
     private double aliquota;
+    public ImpostoIpi(String descricao, double produto, double frete, double seguro, double outrasDespesas, double aliquota) {
+        this.descricao = descricao;
+        this.produto = produto;
+        this.frete = frete;
+        this.seguro = seguro;
+        this.outrasDespesas = outrasDespesas;
+        this.aliquota = aliquota;
+    }
     @Override
     public double calcular() {
         // TODO Auto-generated method stub
